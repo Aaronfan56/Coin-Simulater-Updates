69c0a1c4e25473ace871faef2815182ed87cca86
Aaronfan56 <138613578+Aaronfan56@users.noreply.github.com>
2023-08-04 14:12:02 -0400

    Update AssetsCode.lua

--- AssetsCode.lua
+++ AssetsCode.lua
@@ -1,1 +1,12 @@
+--// UpgradeShop //--
+local players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+local openUpgrades = ReplicatedStorage.Remots.Events:WaitForChild("OpenUpgrades")
+
+script.Parent.Touched:Connect(function(hit)
+	if hit and hit.Parent and hit.Parent:FindFirstChild("Humanoid") then
+		local player = players:GetPlayerFromCharacter(hit.Parent)
+		openUpgrades:FireClient(player)
+	end
+end)
