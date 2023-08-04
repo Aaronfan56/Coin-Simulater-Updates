ce29fb18247cd11272f9141f2935a87b354f47f4
Aaronfan56 <138613578+Aaronfan56@users.noreply.github.com>
2023-08-04 14:17:02 -0400

    Update DSCode.lua

--- DSCode.lua
+++ DSCode.lua
@@ -1,1 +1,9 @@
+--// Kill Script //--
 
+local Players = game:GetService("Players")
+
+script.Parent.Touched:Connect(function(Hit)
+  if Hit and Hit.Parent and Hit.Parent:FindFirstChild("") then
+        local player = Players:GetChildren(Hit.Parent)
+    end
+end)
